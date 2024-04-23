a02ed01dcea36d2a1005fdc3655fe6c37b1798f8
NguyenManh <nguyenmanh200391@gmail.com>
2024-04-23 23:31:33 +0700

    Mathf

--- Assets/Scrip/Graph.cs
+++ Assets/Scrip/Graph.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     [SerializeField] Transform pointPrefab;
-    [SerializeField,Range(10,100)]  int resolution=10;
+    [SerializeField, Range(10, 100)] int resolution = 10;
     Transform[] points;
+    [SerializeField, Range(0,2)] int function;
 
     private void Awake()
     {
@@ -34,7 +35,18 @@ public class Graph : MonoBehaviour
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI*(position.x+time));
+            if (function == 0)
+            {
+                position.y = FunctionLibrary.Wave(position.x, time);
+            }
+            else if (function == 1) 
+            {
+                position.y=FunctionLibrary.MultiWave(position.x, time);
+            }
+            else
+            {
+                position.y=FunctionLibrary.Ripple(position.x, time); 
+            }
             point.localPosition = position;
         }
     }
