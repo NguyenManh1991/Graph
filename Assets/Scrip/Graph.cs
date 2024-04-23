b6e0ff69066422874f8daa75614a3f72a20b7aa3
NguyenManh <nguyenmanh200391@gmail.com>
2024-04-24 05:56:28 +0700

    end

--- Assets/Scrip/Graph.cs
+++ Assets/Scrip/Graph.cs
@@ -8,46 +8,45 @@ public class Graph : MonoBehaviour
     [SerializeField] Transform pointPrefab;
     [SerializeField, Range(10, 100)] int resolution = 10;
     Transform[] points;
-    [SerializeField, Range(0,2)] int function;
-
+    //[SerializeField, Range(0,2)] int function;
+    [SerializeField] FunctionLibrary.FunctionName function;
     private void Awake()
     {
-        var step = 2f / resolution;
-        var position=Vector3.zero;
-        var scale = Vector3.one *step;
-        points = new Transform[resolution];
-        for(int i=0;i<points.Length;i++)
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
+        //var position = Vector3.zero;
+        points = new Transform[resolution * resolution];
+        for (int i = 0; i < points.Length; i++)
         {
-                         
+            //if (x == resolution) {
+            //	x = 0;
+            //	z += 1;
+            //}
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) *step -1f;
-            //position.y = position.x;
-            position.y = position.x * position.x;
-            point.localPosition=position;
+            //position.x = (x + 0.5f) * step - 1f;
+            //position.z = (z + 0.5f) * step - 1f;
+            //point.localPosition = position;
             point.localScale = scale;
-            point.SetParent(transform,false);
+            point.SetParent(transform, false);
         }
     }
     private void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            if (function == 0)
+            if (x == resolution)
             {
-                position.y = FunctionLibrary.Wave(position.x, time);
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
             }
-            else if (function == 1) 
-            {
-                position.y=FunctionLibrary.MultiWave(position.x, time);
-            }
-            else
-            {
-                position.y=FunctionLibrary.Ripple(position.x, time); 
-            }
-            point.localPosition = position;
+            float u = (x + 0.5f) * step - 1f;
+            //float v = (z + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
